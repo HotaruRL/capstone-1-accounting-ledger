bca6886cecec06fddecccd7aa94850378fedf699
Hau Luc <richardluchau@protonmail.com>
2025-04-28 17:01:41 -0700

    * Rearrange menus in Menu.java

--- src/Menu.java
+++ src/Menu.java
@@ -7,70 +7,6 @@ public class Menu {
     // Create an instance of Feature class to use its methods
     Features features = new Features();
 
-    // Home Screen Menu ////////////////////////////////////////////////////////////////////////////////////////////////
-    public void homeMenu(){
-        String userInput = "";
-        while(!userInput.equals("X")){
-            System.out.print("""
-                        -----------------------------------------------------------
-                        ------------------------ Main Menu ------------------------
-                        -----------------------------------------------------------
-                        
-                        [D] - Add Deposit (Credit)
-                        [P] - Make Payment (Debit)
-                        [L] - Show Ledger Menu
-                        [X] - Exit
-                        
-                        Please enter one of these letters ([D], [P], [L], or [X])
-                        to run the corresponding task:
-                        """
-            );
-            userInput = in.nextLine().trim().toUpperCase();
-            switch (userInput){
-                case "D" -> features.addTransaction(true);
-                case "P" -> features.addTransaction(false);
-                case "L" -> ledgerMenu();
-                case "X" -> System.out.println("\nThank you for using this app!");
-                default  -> System.out.println("\nCommand not found. Please try again!");
-
-            }// End of switch
-        }// End of while loop for Home Menu
-        System.out.println("Goodbye!");
-    }// End of Home Menu ////////////////////////////////////////////////////////////////////////////////////////
-
-    // Ledger Menu /////////////////////////////////////////////////////////////////////////////////////////////////////
-
-    public void ledgerMenu(){
-        String userInput = "";
-        while(!userInput.equals("X")){
-            System.out.print("""
-                        -----------------------------------------------------------
-                        ------------------------ Ledger Menu ----------------------
-                        -----------------------------------------------------------
-                        
-                        [A] - Display All Entries
-                        [D] - Display ONLY Deposits
-                        [P] - Display ONLY Payments
-                        [R] - Run Reports + Custom Search
-                        [H] - Go Back to Home Menu
-                        
-                        Please enter one of these letters ([A], [D], [P], [R], or [X])
-                        to run the corresponding task:
-                        """
-            );
-            userInput = in.nextLine().trim().toUpperCase();
-            switch (userInput){
-                case "A" -> features.displayAll();
-                case "D" -> features.displayOnly(true);
-                case "P" -> features.displayOnly(false);
-                case "R" -> reportsMenu();
-                case "H" -> homeMenu();
-                default  -> System.out.println("\nCommand not found. Please try again!");
-
-            }// End of switch
-        }// End of while loop for Ledger Menu
-    }// End of Ledger Menu ////////////////////////////////////////////////////////////////////////////////////////
-
     // Reports Menu /////////////////////////////////////////////////////////////////////////////////////////////////////
 
     public void reportsMenu(){
@@ -106,10 +42,76 @@ public class Menu {
         }// End of while loop for Reports Menu
     }// End of Reports Menu ////////////////////////////////////////////////////////////////////////////////////////
 
+
+    // Ledger Menu /////////////////////////////////////////////////////////////////////////////////////////////////////
+
+    public void ledgerMenu(){
+        String userInput = "";
+        while(!userInput.equals("X")){
+            System.out.print("""
+                        -----------------------------------------------------------
+                        ------------------------ Ledger Menu ----------------------
+                        -----------------------------------------------------------
+                        
+                        [A] - Display All Entries
+                        [D] - Display ONLY Deposits
+                        [P] - Display ONLY Payments
+                        [R] - Run Reports + Custom Search
+                        [H] - Go Back to Home Menu
+                        
+                        Please enter one of these letters ([A], [D], [P], [R], or [H])
+                        to run the corresponding task:
+                        """
+            );
+            userInput = in.nextLine().trim().toUpperCase();
+            switch (userInput){
+                case "A" -> features.displayAll();
+                case "D" -> features.displayOnly(true);
+                case "P" -> features.displayOnly(false);
+                case "R" -> reportsMenu();
+                case "H" -> homeMenu();
+                default  -> System.out.println("\nCommand not found. Please try again!");
+
+            }// End of switch
+        }// End of while loop for Ledger Menu
+    }// End of Ledger Menu ////////////////////////////////////////////////////////////////////////////////////////
+
+
+    // Home Screen Menu ////////////////////////////////////////////////////////////////////////////////////////////////
+    public void homeMenu(){
+        String userInput = "";
+        while(!userInput.equals("X")){
+            System.out.print("""
+                        -----------------------------------------------------------
+                        ------------------------ Main Menu ------------------------
+                        -----------------------------------------------------------
+                        
+                        [D] - Add Deposit (Credit)
+                        [P] - Make Payment (Debit)
+                        [L] - Show Ledger Menu
+                        [X] - Exit
+                        
+                        Please enter one of these letters ([D], [P], [L], or [X])
+                        to run the corresponding task:
+                        """
+            );
+            userInput = in.nextLine().trim().toUpperCase();
+            switch (userInput){
+                case "D" -> features.addTransaction(true);
+                case "P" -> features.addTransaction(false);
+                case "L" -> ledgerMenu();
+                case "X" -> System.out.println("\nThank you for using this app!");
+                default  -> System.out.println("\nCommand not found. Please try again!");
+
+            }// End of switch
+        }// End of while loop for Home Menu
+        System.out.println("Goodbye!");
+    }// End of Home Menu ////////////////////////////////////////////////////////////////////////////////////////
+
 //    Methods for Date
 //    Plain Text//e.g. Previous Year
 //    LocalDate today = LocalDate.now();
 //    LocalDate firstDay = today.minusYears(1).with(firstDayOfYear());
 //    LocalDate lastDay = today.minusYears(1).with(lastDayOfYear());
 //    minusYears method and with method and firstDayOfYear() function are newish
-}// End of Ledger class
+}// End of Menu class
